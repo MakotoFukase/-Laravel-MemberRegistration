39f1998e8ff34be9c42adb20893e57b81fd52717
fukasemakoto <fukase.makoto@trans-cosmos.co.jp>
2019-04-18 15:59:14 +0900

    【CSV入力】foreach最初と最後の行1つのif文にまとめられないかいろいろ

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -3,14 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
-use App\User;
 use Illuminate\Http\Request;
-use App\Exports\UsersExport;
-use App\Imports\UsersImport;
-use Maatwebsite\Excel\Facades\Excel;
+use App\User;
 use Response;
 use SplFileObject;
-use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class CsvController extends Controller
 {
@@ -54,15 +50,6 @@ class CsvController extends Controller
        return Response::make($csv, 200, $headers);
     }
     
-    // CSV出力_Excelクラス使用
-    // 「.csv」だと文字化け不可避
-    /*public function export()
-    {
-        $now = date("YmdHis");
-        return Excel::download(new UsersExport, "users_$now.xlsx");
-    }*/
-
-    
     // CSV入力
     public function import(Request $request)
     {
@@ -72,30 +59,27 @@ class CsvController extends Controller
         // storeメゾット：アップロードファイルの保存
         $now = date("YmdHis");
         $file_name = $request->file('file')->storeAs('import', "$now.csv");
-        // TODO:フルパス取得するやつに変更
-        $file_path = "/home/vagrant/code/Laravel/storage/app/$file_name";
+        $file_path = storage_path("app/$file_name");
 
         // 読み込んだデータをUTF-8に変換して保存
         file_put_contents($file_path, mb_convert_encoding(file_get_contents($file_path), 'UTF-8'));
         $file = new SplFileObject($file_path, "r+b");
         $file->setFlags(SplFileObject::READ_CSV);
 
-        // $registerde_id でDB内のidを取得
-        //$registerde_id = DB::table('users')->take(1)->get(['id']);//->orderBy('id', 'desc')->take(1);//->get(['id']);
-        $registerde_id = DB::table('users')->orderBy('id', 'desc')->first()->id;
-
-        //$registerde_id = $registerde_id->id;
+        // $existing_id でDB内のidを取得
+        $existing_id = DB::table('users')->orderBy('id', 'desc')->first()->id;
 
         $row_count = 1;
-        // オブジェクトのままforeach
         foreach ($file as $key => $row){
-            // 最終行の処理(最終行が空っぽの場合の対策)
-            if ($row === [null]) continue; 
+            // 最終行
+            //if ($row === [null]) continue; 
 
-
-            if ($file->key() > 0){
+            if ($file->key() == null) {
+                continue;
+            }
+            elseif ($file->key() > 0) {
                 // CSVに入っているidがDBに入っているidより大きければ、無効にする
-                if ($row[0] > $registerde_id) {
+                if ($row[0] > $existing_id) {
                     $row[0] = null;
                 }
                 
@@ -108,9 +92,8 @@ class CsvController extends Controller
                 }
 
                 User::updateOrCreate(
-                    ['id' => (int)$row[0]], // $registerde_id でDB内のidを取得する
+                    ['id' => (int)$row[0]],
                     [
-                        //'id'        => 0,
                         'name'      => $row[1], 
                         'email'     => $row[2], 
                         'password'  => $row[3], 
@@ -123,26 +106,7 @@ class CsvController extends Controller
                 );
             }
         }
-
-        /*foreach ($file as $key => $row) {
-            if ($row === [null]) continue; 
-            // 行番号を取得し、最初の行のみ除外
-            if ($file->key() > 0){
-                //$date = date('Y-m-d', strtotime($row[4]));
-                var_dump((object)$row);
-            }
-        }*/
-        //return view('users.test', ['registerde_id'=>$registerde_id]);
+        //return view('users.test', ['existing_id'=>$existing_id]);
         return redirect ('/list');
     }
-
-    // アップデートできないので却下
-    /*public function import(Request $request)
-    {
-        $file = $request->file('file');
-        Excel::import(new UsersImport, $file);
-        return redirect ('/list');
-
-        //return view('users.test', ['file'=>$file]);
-    }*/
 }
